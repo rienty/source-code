7058c4734a10d007f20c43d92dab311d697db65a
Ren <tianyin.rien@gmail.com>
2022-08-05 08:05:06 +0800

    change dmenu xyz patch

--- dmenu/config.h
+++ dmenu/config.h
@@ -3,14 +3,15 @@
 
 static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = { "GohuFont Nerd Font Mono:size=19" };
+static const char *fonts[] = { "GohuFont Nerd Font Mono:size=18" };
 
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*               fg         bg         */
 	[SchemeNorm] = { "#ebdbb2", "#282828" },
-	[SchemeSel] = { "#ebdbb2", "#665c54" },
-	[SchemeOut] = { "#ebdbb2", "#d65d0e" },
+	[SchemeSel] = { "#282828", "#d79921" },
+	[SchemeOut] = { "#8ec07c", "#d65d0e" },
+    [SchemeMid] = { "#ebdbb2", "#3c3836" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
