55bb022453ff8aa33b54ffb15115415f748af982
Ren <tianyin.rien@gmail.com>
2022-08-22 09:07:26 +0800

    update

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "TerminessTTF Nerd Font:size=20:antialias=true:autohint=true";
+static char *font = "TerminessTTF Nerd Font:size=19:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
